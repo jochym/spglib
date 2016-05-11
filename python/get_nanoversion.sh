de0ace8fb42cefa99c7f66bbfcdbd78cc1401e4d
Pawel T. Jochym <jochym@wolf.ifj.edu.pl>
2016-05-11 15:15:45 +0200

    Use proper temp dir and clean up after yourself

--- python/get_nanoversion.sh
+++ python/get_nanoversion.sh
@@ -2,8 +2,12 @@
 
 br=`git branch |tail -n1`
 read o u f <<< `git remote -v |grep fetch`
-git clone $u TTTT
-cd TTTT
+TD=`mktemp -d`
+WD=`pwd`
+git clone $u $TD
+cd $TD
 echo $u $br
 git checkout $br
-git describe --tags --dirty | sed -e 's/\([.0-9]*\)-\(.*\)-g.*/\2/' -e 's/^v//g' >../__nanoversion__.txt
+git describe --tags --dirty | sed -e 's/\([.0-9]*\)-\(.*\)-g.*/\2/' -e 's/^v//g' >$WD/__nanoversion__.txt
+cd $WD
+rm -rf "$TD"
