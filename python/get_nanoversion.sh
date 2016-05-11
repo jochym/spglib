6e531e00e1988c8e936ca95f1faaef51cf838dab
Pawel T. Jochym <jochym@wolf.ifj.edu.pl>
2016-05-11 15:06:38 +0200

    Try to work around git describe quirks to get consistent nanoversion

--- python/get_nanoversion.sh
+++ python/get_nanoversion.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-git describe --tags --dirty | sed -e 's/\([.0-9]*\)-\(.*\)-g.*/\2/' -e 's/^v//g' >__nanoversion__.txt
+br=`git branch |tail -n1`
+read o u f <<< `git remote -v |grep fetch`
+git clone $u TTTT
+cd TTTT
+echo $u $br
+git checkout $br
+git describe --tags --dirty | sed -e 's/\([.0-9]*\)-\(.*\)-g.*/\2/' -e 's/^v//g' >../__nanoversion__.txt
